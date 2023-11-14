dc8042dad28d9c3ff23d6b5e93686404f01b72f6
ChaoII <849453582@qq.com>
2023-11-14 17:01:35 +0800

    add splash screen

---
+++ widgets/MySplashScreen.cpp
@@ -0,0 +1,5 @@
+//
+// Created by AC on 2023/11/14.
+//
+
+#include "MySplashScreen.h"
---
+++ widgets/MySplashScreen.h
@@ -0,0 +1,14 @@
+//
+// Created by AC on 2023/11/14.
+//
+
+#ifndef FRAM_MYSPLASHSCREEN_H
+#define FRAM_MYSPLASHSCREEN_H
+
+
+class MySplashScreen {
+
+};
+
+
+#endif //FRAM_MYSPLASHSCREEN_H
